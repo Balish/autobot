4746e03389f5f0c8f98ac4deab9b66fd3331a691
Timur Khusainov <timur.husainov@gmail.com>
2013-08-26 00:24:11 +0500

    Autoselling website

---
+++ protected/views/site/index.php
@@ -0,0 +1,171 @@
+<!-- Auto Catalog -->
+        <div id="auto_catalog">
+            <div class="categories">Categories
+                <ul>
+                        <li>Light</li>
+                        <li>Heavy</li>
+                        <li>Semi-heavy</li>
+                        <li>Semi-light</li>
+                        <li>Extra-heavy</li>
+                    </ul>
+                    <div class="advertisementB"></div>
+            </div>
+            <div id="shadow_left"><img src="img/shadow_left.png" width="100%" /></div>
+            <div class="announcement">Announcements by marks
+                    <ul id="triple">
+                        <li>Audi</li>
+                        <li>Audi</li>
+                        <li>Audi</li>
+                        <li>Audi</li>
+                        <li>Audi</li>
+                        <li>Audi</li>
+                        <li>Audi</li>
+                        <li>Audi</li>
+                        <li>Audi</li>
+                        <li>Audi</li>
+                        <li>Audi</li>
+                        <li>Audi</li>
+                        <li>Audi</li>
+                        <li>Audi</li>
+                        <li>Audi</li>
+                        <li>Audi</li>
+                        <li>Audi</li>
+                        <li>Audi</li>
+                        <li>Audi</li>
+                        <li>Audi</li>
+                        <li>Audi</li>
+                        <li>Audi</li>
+                        <li>Audi</li>
+                        <li>Audi</li>
+                        <li>Audi</li>
+                        <li>Audi</li>
+                        <li>Audi</li>
+                        <li>Audi</li>
+                        <li>Audi</li>
+                        <li>Audi</li>
+                        <li>Audi</li>
+                        <li>Audi</li>
+                        <li>Audi</li>
+                        <li>Audi</li>
+                        <li>Audi</li>
+                        <li>Audi</li>
+                        <li>Audi</li>
+                        <li>Audi</li>
+                        <li>Audi</li>
+                    </ul>
+            </div>
+            <div id="shadow_right"><img src="img/shadow_right.png" width="100%" /></div>
+            <div id="search_auto_container">
+            <div class="search_auto"><div class="tag_corner"></div><div class="car"></div><p>Поиск подержанного авто</p></div>
+                <div class="select mark">
+                <select>
+                        <option>Apples</option>
+                        <option>Pineapples</option>
+                        <option>Chocklate</option>
+                        <option>Pancakes</option>
+                </select>
+                </div>
+                <div class="select model">
+                <select>
+                        <option>Apples</option>
+                        <option>Pineapples</option>
+                        <option>Chocklate</option>
+                        <option>Pancakes</option>
+                </select>
+                </div>
+                <div class="city">
+                    <input type="text" placeholder="Town"/>
+                </div>
+                <div class="year">
+                    <span class="left">Год от</span><input type="text" placeholder="1988"/>
+                    <span>до</span><input type="text" placeholder="2013"/>
+                </div>
+                <div class="price">
+                    <span>Цена от</span><input type="text" placeholder="10000"/>
+                    <span>до</span><input type="text" placeholder="2900000"/>
+                </div>
+                <div class="photo_required">
+                    <input type="checkbox" checked/><span>Только с фото</span>
+                </div>
+                <div class="advanced_search">
+                   Расширенный поиск
+                </div>
+                <div class="search_button"><p>поиск</p></div>
+            </div>
+        </div>
+        
+        <!-- Special Offers -->
+        <div id="special_offers">
+            <div class="left">
+                <strong>Автоорганизации</strong>
+                <li>Автозапчасти</li>
+                <li>Автозапчасти</li>
+                <li>Автозапчасти</li>
+                <li>Автозапчасти</li>
+                <li>Автозапчасти</li>
+                <li>Автозапчасти</li>
+                <li>Автозапчасти</li>
+                <li>Автозапчасти</li>
+                <li>Автозапчасти</li>
+                <li>Автозапчасти</li>
+                <br />
+                <br />
+                <br />
+                <strong>Автовладельцам</strong>
+                <li>Автозапчасти</li>
+                <li>Автозапчасти</li>
+                <li>Автозапчасти</li>                
+                <br />
+                <div class="advertisementB"></div>
+            </div>
+            <div class="center">
+                <strong>Спецпредложения</strong>
+                <br />
+                <li><img src="img/subaru.png" width="100%">Subaru Foreste II, 2007<br/>560 000 р.<br /><i>Санкт-Петербург</i></li>
+                <li><img src="img/subaru.png" width="100%">Subaru Foreste II, 2007<br/>560 000 р.<br /><i>Санкт-Петербург</i></li>
+                <li><img src="img/subaru.png" width="100%">Subaru Foreste II, 2007<br/>560 000 р.<br /><i>Санкт-Петербург</i></li>
+                <li><img src="img/subaru.png" width="100%">Subaru Foreste II, 2007<br/>560 000 р.<br /><i>Санкт-Петербург</i></li>
+                <li><img src="img/subaru.png" width="100%">Subaru Foreste II, 2007<br/>560 000 р.<br /><i>Санкт-Петербург</i></li>
+                <li><img src="img/subaru.png" width="100%">Subaru Foreste II, 2007<br/>560 000 р.<br /><i>Санкт-Петербург</i></li>
+                <li><img src="img/subaru.png" width="100%">Subaru Foreste II, 2007<br/>560 000 р.<br /><i>Санкт-Петербург</i></li>
+                <li><img src="img/subaru.png" width="100%">Subaru Foreste II, 2007<br/>560 000 р.<br /><i>Санкт-Петербург</i></li>
+                <li><img src="img/subaru.png" width="100%">Subaru Foreste II, 2007<br/>560 000 р.<br /><i>Санкт-Петербург</i></li>
+                <li><img src="img/subaru.png" width="100%">Subaru Foreste II, 2007<br/>560 000 р.<br /><i>Санкт-Петербург</i></li>
+                <li><img src="img/subaru.png" width="100%">Subaru Foreste II, 2007<br/>560 000 р.<br /><i>Санкт-Петербург</i></li>
+                <li><img src="img/subaru.png" width="100%">Subaru Foreste II, 2007<br/>560 000 р.<br /><i>Санкт-Петербург</i></li>
+                <li><img src="img/subaru.png" width="100%">Subaru Foreste II, 2007<br/>560 000 р.<br /><i>Санкт-Петербург</i></li>
+                <li><img src="img/subaru.png" width="100%">Subaru Foreste II, 2007<br/>560 000 р.<br /><i>Санкт-Петербург</i></li>
+                <li><img src="img/subaru.png" width="100%">Subaru Foreste II, 2007<br/>560 000 р.<br /><i>Санкт-Петербург</i></li>
+                <li><img src="img/subaru.png" width="100%">Subaru Foreste II, 2007<br/>560 000 р.<br /><i>Санкт-Петербург</i></li>
+            </div>
+            <div class="right">
+                <div class="advertisementC"></div>
+                <div class="advertisementB"></div>
+            </div>
+            <div class="clear"></div>
+        
+            <!-- News -->
+            <strong class="news">Новости сайта</strong>
+            <div class="bottom">
+                <div class="news_block">
+                    <div class="news_title">График работы</div>
+                    <div class="news_content">1, 2, 5, 6, 7, 8 января - выходные дни. 3, 4 января мы работаем с 10 до 18.30.12.2012</div>
+                </div>
+                <div class="news_block">
+                    <div class="news_title">График работы</div>
+                    <div class="news_content">1, 2, 5, 6, 7, 8 января - выходные дни. 3, 4 января мы работаем с 10 до 18.30.12.2012</div>
+                </div>
+                <div class="news_block">
+                    <div class="news_title">График работы</div>
+                    <div class="news_content">1, 2, 5, 6, 7, 8 января - выходные дни. 3, 4 января мы работаем с 10 до 18.30.12.2012</div>
+                </div>
+                <div class="news_block last">
+                    <div class="news_title">График работы</div>
+                    <div class="news_content">1, 2, 5, 6, 7, 8 января - выходные дни. 3, 4 января мы работаем с 10 до 18.30.12.2012</div>
+                </div>
+            </div>
+        </div>
+        <div class="clear"></div>
+        <!-- Advertisement A -->
+        <div class="advertisementA bottom"><img src="img/advertisement A.png" width="100%" /></div>
+        <div class="clear"></div>
